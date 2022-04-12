c71be8dda251a111f763ba21dfd5a06e85df2b4a
Andrew Ross <andrew.ross.mail@gmail.com>
2022-04-12 17:18:41 -0400

    Add package implementation

---
+++ src/vai_jobspec_tools.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+import contextlib
+import logging
+import os
+import os.path as op
+import tempfile
+from pathlib import Path
+from typing import Iterator
+from urllib.parse import urlparse
+
+
+__version__ = "0.1.0"
+
+__all__ = (
+    "configure_logger",
+    "optional_temporary_directory",
+    "prepare_dst_uri",
+    "lowercase_alnum",
+)
+
+
+def configure_logger(
+    logger: logging.Logger, *, verbosity: int = 0, level: int | None = None
+):
+    _level = max(logging.WARNING - verbosity * 10, 0) if level is None else level
+    logger.setLevel(_level)
+    fmt = "%(asctime)s - %(levelname)s - %(module)s:%(lineno)d - %(message)s"
+    formatter = logging.Formatter(fmt=fmt)
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+@contextlib.contextmanager
+def optional_temporary_directory(workdir: str | Path | None = None) -> Iterator[Path]:
+    if workdir is None:
+        with tempfile.TemporaryDirectory() as _workdir:
+            yield Path(_workdir)
+    elif Path(workdir).is_dir():
+        yield Path(workdir)
+    else:
+        raise ValueError(
+            f"'workdir' must be an existing directory or None. Found [{workdir}]"
+        )
+
+
+def prepare_dst_uri(
+    uri: str,
+    subject_id: str,
+    session_id: str,
+    pipeline_name: str,
+    pipeline_version: str,
+) -> str:
+    pipeline_folder = _create_pipeline_folder_path(
+        subject_id, session_id, pipeline_name, pipeline_version
+    )
+    dst_uri = op.join(uri, pipeline_folder)
+    # if local path then create the intermediate directories
+    _uri = urlparse(dst_uri)
+    if _uri.scheme in ("", "file"):
+        os.makedirs(_uri.path, exist_ok=True)
+
+    return dst_uri
+
+
+def _create_pipeline_folder_path(
+    subject_id: str, session_id: str, pipeline_name: str, pipeline_version: str
+):
+    return op.join(subject_id, session_id, pipeline_name, pipeline_version)
+
+
+def lowercase_alnum(s: str) -> str:
+    """Transform a string so that it contains only lowercase alphanumeric characters.
+    Examples:
+        >>> lowercase_alnum('Hello, World!')
+        'helloworld'
+        >>> lowercase_alnum('A.String.0.1.2')
+        'astring012'
+        >>> lowercase_alnum('HCA9865005_V1_MR')
+        'hca9865005v1mr'
+    """
+    return "".join(c.lower() for c in s if c.isalnum())
---
+++ tests/e2e_test.py
@@ -0,0 +1,157 @@
+import logging
+import os.path as op
+from pathlib import Path
+from typing import NamedTuple
+from urllib.parse import urlparse
+from urllib.parse import urlunparse
+from uuid import uuid4
+
+import pytest
+
+from vai_jobspec_tools import configure_logger
+from vai_jobspec_tools import optional_temporary_directory
+from vai_jobspec_tools import prepare_dst_uri
+
+
+@pytest.fixture
+def logger():
+    return logging.getLogger(str(uuid4()))
+
+
+def test_configure_logger_with_defaults(logger: logging.Logger):
+    assert logger.level == logging.NOTSET
+    assert len(logger.handlers) == 0
+
+    configure_logger(logger)
+
+    assert logger.level == logging.WARNING
+    assert len(logger.handlers) == 1
+
+
+@pytest.mark.parametrize(
+    ("verbosity", "expected_level"),
+    [
+        (0, logging.WARNING),
+        (1, logging.INFO),
+        (2, logging.DEBUG),
+        (3, logging.NOTSET),
+        (8, logging.NOTSET),
+    ],
+)
+def test_configure_logger_verbosity(logger: logging.Logger, verbosity, expected_level):
+    assert logger.level == logging.NOTSET
+    assert len(logger.handlers) == 0
+
+    configure_logger(logger, verbosity=verbosity)
+
+    assert logger.level == expected_level
+    assert len(logger.handlers) == 1
+
+
+@pytest.mark.parametrize(
+    ("level", "expected_level"),
+    [
+        (logging.WARNING, logging.WARNING),
+        (logging.INFO, logging.INFO),
+        (logging.DEBUG, logging.DEBUG),
+        (logging.NOTSET, logging.NOTSET),
+    ],
+)
+def test_configure_logger_level(logger: logging.Logger, level, expected_level):
+    assert logger.level == logging.NOTSET
+    assert len(logger.handlers) == 0
+
+    configure_logger(logger, level=level)
+
+    assert logger.level == expected_level
+    assert len(logger.handlers) == 1
+
+
+@pytest.mark.parametrize(
+    ("level", "expected_level"),
+    [
+        (logging.WARNING, logging.WARNING),
+        (logging.INFO, logging.INFO),
+        (logging.DEBUG, logging.DEBUG),
+        (logging.NOTSET, logging.NOTSET),
+    ],
+)
+def test_configure_logger_level_params_override_verbosity_param(
+    logger: logging.Logger, level, expected_level
+):
+    assert logger.level == logging.NOTSET
+    assert len(logger.handlers) == 0
+
+    # verbosity > 2 means that logger.level should be UNSET (if level wasn't specified)
+    configure_logger(logger, level=level, verbosity=42)
+
+    assert logger.level == expected_level
+    assert len(logger.handlers) == 1
+
+
+def test_optional_temporary_directory_default():
+    with optional_temporary_directory() as tmpdir:
+        assert tmpdir.exists()
+    assert not tmpdir.exists()
+
+
+def test_optional_temporary_directory_explicit_directory(tmp_path: Path):
+    with optional_temporary_directory(tmp_path) as tmpdir:
+        assert tmpdir.exists()
+        assert tmpdir == tmp_path
+
+    assert tmpdir.exists()
+    assert tmpdir == tmp_path
+
+
+def test_optional_temporary_directory_raises_if_param_does_not_exist():
+    with pytest.raises(ValueError):
+        with optional_temporary_directory("/some/fake/dir/xyz/") as tmpdir:
+            ...
+
+
+def test_optional_temporary_directory_raises_if_param_exists_but_not_dir(
+    tmp_path: Path,
+):
+    tmp_file = tmp_path / "file.txt"
+    tmp_file.write_text("")
+
+    with pytest.raises(ValueError):
+        with optional_temporary_directory(tmp_file) as tmpdir:
+            ...
+
+
+class PrepareDstUriArgs(NamedTuple):
+    uri: str
+    subject_id: str
+    session_id: str
+    pipeline_name: str
+    pipeline_version: str
+
+
+@pytest.mark.parametrize("scheme", ["", "file"])
+def test_prepare_dst_uri_local_file(scheme: str, tmp_path: Path):
+    components = (scheme, "", str(tmp_path / "a" / "b"), "", "", "")
+    uri = urlunparse(components)
+    parts = PrepareDstUriArgs(uri, "sub1", "ses1", "fake-pipe", "0.0.0")
+    expected_uri = op.join(*parts)
+    expected_path = urlparse(expected_uri).path
+
+    assert not op.exists(expected_path)
+
+    dst_uri = prepare_dst_uri(*parts)
+
+    assert dst_uri == expected_uri
+    assert op.exists(expected_path)
+
+
+@pytest.mark.parametrize("uri", ["gs://bucket", "gs://bucket/sub/folder"])
+@pytest.mark.parametrize("suffix", ["", "/"])
+def test_prepare_dst_uri_remote_file(uri: str, suffix: str):
+    _uri = f"{uri}{suffix}"
+    parts = PrepareDstUriArgs(_uri, "sub1", "ses1", "fake-pipe", "0.0.0")
+    expected_uri = op.join(*parts)
+
+    dst_uri = prepare_dst_uri(*parts)
+
+    assert dst_uri == expected_uri
